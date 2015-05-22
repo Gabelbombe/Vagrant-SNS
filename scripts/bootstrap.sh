a1e826137e835307042e842cdc928e97dcc97e78
ehime <dodomeki@gmail.com>
2015-05-22 15:08:49 -0700

    Missing required module CURL

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -123,9 +123,10 @@ service apache2 restart
 source /etc/apache2/envvars
 
 
-# Install PHP FastCGI Process Manager
+# Install PHP modules
 # --------------------
-apt-get install -y php5-fpm php5-cli
+apt-get install -y php5-fpm php5-cli php5-curl
+
 sed -i -e 's/www-data/vagrant/g'                        /etc/php5/fpm/pool.d/www.conf
 sed -i -e 's~/var/run/php5-fpm.sock~127.0.0.1:9000~g'   /etc/php5/fpm/pool.d/www.conf
 
