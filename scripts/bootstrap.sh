43cbcb9191e7eb57b3b826b56ccbe182f4f27ce5
ehime <dodomeki@gmail.com>
2015-05-22 15:03:45 -0700

    Requires CLI

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -124,10 +124,19 @@ source /etc/apache2/envvars
 
 # Install PHP FastCGI Process Manager
 # --------------------
-apt-get install -y php5-fpm
+apt-get install -y php5-fpm php5-cli
 sed -i -e 's/www-data/vagrant/g'                        /etc/php5/fpm/pool.d/www.conf
 sed -i -e 's~/var/run/php5-fpm.sock~127.0.0.1:9000~g'   /etc/php5/fpm/pool.d/www.conf
 
 service php5-fpm restart
 
 git clone $URL $APPDIR
+
+
+# Install Composer
+# --------------------
+su vagrant -c "
+  curl -sS https://getcomposer.org/installer |php
+  sudo mv composer.phar /bin/composer
+  cd $APPDIR && composer install
+"
